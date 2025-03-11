44f5bf12022c9d722e3c16ca665068280a63c946
Julia Cheperis <julia.cheperis@dataart.com>
2025-03-11 17:55:22 +0100

    fix

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -3,14 +3,16 @@ import * as contactsService from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
 
 export const getAllContacts = async (req, res) => {
-  const contacts = await contactsService.listContacts();
+  const { id: owner } = req.user;
+  const contacts = await contactsService.listContacts({ owner });
 
   res.json(contacts);
 };
 
 export const getOneContact = async (req, res) => {
   const { id } = req.params;
-  const contact = await contactsService.getContactById(id);
+  const { id: owner } = req.user;
+  const contact = await contactsService.getContactById({ id, owner });
 
   if (!contact) {
     throw HttpError(404, "Not found");
@@ -21,7 +23,8 @@ export const getOneContact = async (req, res) => {
 
 export const deleteContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contactsService.removeContact(id);
+  const { id: owner } = req.user;
+  const result = await contactsService.removeContact({ id, owner });
 
   if (!result) {
     throw HttpError(404, `Not found`);
@@ -32,17 +35,24 @@ export const deleteContact = async (req, res) => {
 
 export const createContact = async (req, res) => {
   const { name, email, phone } = req.body;
-  const result = await contactsService.addContact(name, email, phone);
+  const { id: owner } = req.user;
+  const result = await contactsService.addContact({
+    name,
+    email,
+    phone,
+    owner,
+  });
 
   res.status(201).json(result);
 };
 
 export const updateContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contactsService.updateContact(id, req.body);
+  const { id: owner } = req.user;
+  const result = await contactsService.updateContact({ id, owner }, req.body);
 
   if (!result) {
-    throw HttpError(404, 'Not found');
+    throw HttpError(404, "Not found");
   }
 
   res.json(result);
